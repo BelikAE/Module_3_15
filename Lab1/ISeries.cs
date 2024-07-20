4ee4bce1287512049114bfed497447a12f39c182
BelikAE <BelikAE@HOME-PC>
2024-07-20 12:26:02 +1000

    Выдача

---
+++ Lab1/ISeries.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+
+    class ArithProgression : ISeries
+    {
+        int starValue;
+        int step;
+        int currentValue;
+        public int GetNext()
+        {
+            currentValue += step;
+            return currentValue;
+        }
+
+        public void Reset()
+        {
+            currentValue = starValue;
+        }
+
+        public void SetStart(int x)
+        {
+            starValue = x;
+            currentValue = starValue;
+        }
+
+        public void SetStep(int s)
+        {
+            step = s;
+        }
+
+    }
+    class GeomProgression : ISeries
+    {
+        int starValue;
+        int step;
+        int currentValue;
+        public int GetNext()
+        {
+            currentValue *= step;
+            return currentValue;
+        }
+
+        public void Reset()
+        {
+            currentValue = starValue;
+        }
+
+        public void SetStart(int x)
+        {
+            starValue = x;
+            currentValue = starValue;
+        }
+
+        public void SetStep(int s)
+        {
+            step = s;
+        }
+    }
+}
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,6 +10,28 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            ArithProgression arProg = new ArithProgression();
+            arProg.SetStart(0);
+            arProg.SetStep(5);
+            Console.WriteLine(arProg.GetNext());
+            Console.WriteLine(arProg.GetNext());
+            Console.WriteLine(arProg.GetNext());
+            Console.WriteLine(arProg.GetNext());
+            arProg.Reset();
+            Console.WriteLine(arProg.GetNext());
+            Console.WriteLine(arProg.GetNext());
+
+            Console.WriteLine();
+            GeomProgression geomProg = new GeomProgression();
+            geomProg.SetStart(3);
+            geomProg.SetStep(10);
+            Console.WriteLine(geomProg.GetNext());
+            Console.WriteLine(geomProg.GetNext());
+            Console.WriteLine(geomProg.GetNext());
+            geomProg.Reset();
+            Console.WriteLine(geomProg.GetNext());
+            Console.WriteLine(geomProg.GetNext());
+            Console.ReadKey();
         }
     }
 }
